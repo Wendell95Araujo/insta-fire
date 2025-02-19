6c414452c2b425e28c1ba4e0407080b92f047973
Wendell Araujo <wendell170695@gmail.com>
2025-02-19 18:58:33 -0300

    Mostrar quantidade de curtidas e comentário na galeria

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,23 @@ $(document).ready(() => {
 
   for (let i = 1; i <= totalImages; i++) {
     const imageUrl = `assets/img/${i}.webp`;
-    galleryContainer.append(`
+
+    const postRef = firebase.firestore().collection("posts").doc(String(i));
+    postRef.get().then((doc) => {
+      const data = doc.exists ? doc.data() : { likes: 0, comments: [] };
+      const likes = data.likes || 0;
+      const comments = data.comments?.length || 0;
+
+      galleryContainer.append(`
         <div class="gallery-item">
           <img src="${imageUrl}" alt="Foto ${i}" data-id="${i}" class="gallery-img">
+          <div class="image-overlay">
+            <span><i class="fas fa-fire"></i> ${likes}</span>
+            <span><i class="far fa-comment"></i> ${comments}</span>
+          </div>
         </div>
       `);
+    });
   }
 
   // Ao clicar, redireciona para a página de visualização
